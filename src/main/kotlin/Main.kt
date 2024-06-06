ed19b6ed5a948a9c634a302523838ab378691ca8
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-06-05 21:42:39 -0300

    aula1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -16,9 +16,19 @@ fun main() {
     val json = response.body()
     println(json)
 
-    val meuJogo = Jogo()
-    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
-    meuJogo.capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1654156906"
+    val meuJogo = Jogo(
+        "Batman: Arkhan Asylum Game of the Year Edition",
+        "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/header.jpg?t=1602606827"
+    )
+
+
     println(meuJogo)
+
+    val novoJogo = Jogo(
+        capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/header.jpg?t=1602606827",
+        titulo = "Batman: Arkhan Asylum Game of the Year Edition"
+    )
+
+    println(novoJogo)
 
 }
