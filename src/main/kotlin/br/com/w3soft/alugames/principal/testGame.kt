97f99b2f6ab770eee1e8894025d7ebb2548b11fb
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-06-11 22:32:00 -0300

    get e set

---
+++ src/main/kotlin/br/com/w3soft/alugames/principal/testGame.kt
@@ -0,0 +1,24 @@
+import br.com.w3soft.alugames.modelo.Gamer
+
+fun main() {
+    val gamer1 = Gamer("Welbert", "welbertsoares@outlook.com")
+    println(gamer1)
+
+    val gamer2 = Gamer(
+        "Jeni",
+        "jeni@emal.com",
+        "01/01/1990",
+        "jeni")
+
+    println(gamer2)
+
+    gamer1.let {
+        it.dataNascimento = "27/02/1999"
+        it.usuario = "W3Nitro"
+
+    }.also {
+        println(gamer1.idInterno)
+    }
+
+    println(gamer1)
+}
