cb7eac426bc60243c128ce2c7e03dfaad36bfcbb
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-06-12 13:08:25 -0300

    busca com repeticao do while

--- src/main/kotlin/br/com/w3soft/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/w3soft/alugames/modelo/Gamer.kt
@@ -1,5 +1,6 @@
 package br.com.w3soft.alugames.modelo
 
+import org.example.br.com.w3soft.alugames.modelo.Jogo
 import kotlin.random.Random
 
 data class Gamer(var nome: String, var email:String) {
@@ -13,6 +14,7 @@ data class Gamer(var nome: String, var email:String) {
         }
     var idInterno: String? = null
         private set
+    val jogosBuscados:MutableList<Jogo> = mutableListOf<Jogo>()
 
     constructor(nome:String, email:String, dataNascimento:String, usuario:String) :
             this(nome, email) {
--- src/main/kotlin/br/com/w3soft/alugames/principal/Main.kt
+++ src/main/kotlin/br/com/w3soft/alugames/principal/Main.kt
@@ -6,39 +6,44 @@ import java.util.*
 
 fun main() {
     val leitura = Scanner(System.`in`)
-    println("Digite um código de jogo para buscar:")
-    val busca = leitura.nextLine()
 
-    val buscaApi = ConsumoApi()
-    val informacaoJogo = buscaApi.buscaJogo(busca)
+    do {
+        println("Digite um código de jogo para buscar:")
+        val busca = leitura.nextLine()
 
-    var meuJogo: Jogo? = null
+        val buscaApi = ConsumoApi()
+        val informacaoJogo = buscaApi.buscaJogo(busca)
 
-    val resultado = runCatching {
-        meuJogo = Jogo(
-            informacaoJogo.info.title,
-            informacaoJogo.info.thumb)
-    }
+        var meuJogo: Jogo? = null
 
-    resultado.onFailure {
-        println("Jogo inexistente, tente novamente com outro código.")
-    }
-
-    resultado.onSuccess {
-        println("Deseja inserir uma descrição personalizada para o jogo? S/N")
-        val opcao = leitura.nextLine()
-        if (opcao.equals("s", true)) {
-            println("Insira a descrição:")
-            val descricaoPersonalizada = leitura.nextLine()
-            meuJogo?.descricao = descricaoPersonalizada
-        } else {
-            meuJogo?.descricao = meuJogo?.titulo
+        val resultado = runCatching {
+            meuJogo = Jogo(
+                informacaoJogo.info.title,
+                informacaoJogo.info.thumb)
         }
 
-        println(meuJogo)
-    }
+        resultado.onFailure {
+            println("Jogo inexistente, tente novamente com outro código.")
+        }
 
-    resultado.onSuccess {
-        println("Jogo encontrado com sucesso!")
-    }
+        resultado.onSuccess {
+            println("Deseja inserir uma descrição personalizada para o jogo? S/N")
+            val opcao = leitura.nextLine()
+            if (opcao.equals("s", true)) {
+                println("Insira a descrição:")
+                val descricaoPersonalizada = leitura.nextLine()
+                meuJogo?.descricao = descricaoPersonalizada
+            } else {
+                meuJogo?.descricao = meuJogo?.titulo
+            }
+
+            println(meuJogo)
+        }
+
+        println("Deseja buscar outro jogo? S/N")
+        val resposta = leitura.nextLine()
+
+    } while (resposta.equals("s", true))
+
+    println("Jogo encontrado com sucesso!")
 }
